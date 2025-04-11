50df415c136a475727e8e028e3d3df7168261c4d
Wilbert <devbert7@gmail.com>
2025-04-11 15:03:56 +0700

    Edit .env setup database, edit user migration (add field role), make and run AdminUserSeeder.php

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'devbert7@gmail.com',
+            'password' => Hash::make('admin400500'),
+            'role' => 'admin',
+        ]);
+    }
+}
