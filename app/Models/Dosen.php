34bc0ae9088d92472f2d828b11ad44ee49c36f79
Wilbert <devbert7@gmail.com>
2025-04-11 22:01:59 +0700

    Create Dosen model and migration

---
+++ app/Models/Dosen.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Dosen extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dosen';
+
+    protected $fillable = [
+        'user_id',
+        'nidn',
+        'nama',
+        'alamat',
+        'no_hp',
+    ];
+
+    // Relasi ke User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
